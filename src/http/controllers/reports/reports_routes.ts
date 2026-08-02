240c6e2eaa669a0de6e810024a33fd3334b02a6f
Gabriel Pirovane Miguel <gabriel.balique@gmail.com>
2026-08-02 00:44:34 -0300

    feat: add user role verification for project report route

--- src/http/controllers/reports/reports_routes.ts
+++ src/http/controllers/reports/reports_routes.ts
@@ -1,7 +1,9 @@
 import { verifyJwt } from '@/http/middlewares/verify_jwt.js'
 import type { FastifyInstance } from 'fastify'
 import { makeReportProjects } from './make_report_projects_controller.js'
+import { verifyUserIdOrRole } from '@/http/middlewares/verify_user_id_or_role.js'
+import { USER_ROLE } from '@/@types/prisma/browser.js'
 
 export function reportsRoutes(app: FastifyInstance) {
-    app.get('/projects', { onRequest: [verifyJwt] }, makeReportProjects)
+    app.get('/projects', { onRequest: [verifyJwt, verifyUserIdOrRole([USER_ROLE.admin])] }, makeReportProjects)
 }
